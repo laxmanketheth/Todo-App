cf8a5e1c580015f7cf666758e6e3fa20e7d328f7
Laxman <133135720+laxmanketheth@users.noreply.github.com>
2024-03-04 22:16:11 +0800

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -8,7 +8,7 @@ dotenv.config();
 const cors = require('cors');
 app.use(cors(
     {
-        origin: ["https://todo-app-frontend-orpin.vercel.app/"],
+        origin: ["https://todo-app-frontend-orpin.vercel.app"],
         methods: ["POST", "GET",  "DELETE"],
         credentials: true
     }
