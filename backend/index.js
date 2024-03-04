814655bc4c1c7cbdd685f86c46c1ceb1f563bcd8
Laxman <laxmanketheth76@gmail.com>
2024-03-04 21:05:37 +0800

    added vercel.json file

--- backend/index.js
+++ backend/index.js
@@ -6,7 +6,13 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 const cors = require('cors');
-app.use(cors());
+app.use(cors(
+    {
+        origin: ["https://deploy-mern-1whq.vercel.app"],
+        methods: ["POST", "GET",  "DELETE"],
+        credential: true
+    }
+));
 
 const bodyParser = require('body-parser');
 app.use(bodyParser.json());
